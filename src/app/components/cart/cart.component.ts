eaaa6d67b7dfc424c2785ec3642178c99cbbc572
Samreen Fathima Farooq <yoyosamreen@gmail.com>
2025-02-04 13:15:49 +0530

    cart over

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,7 +1,7 @@
 // src/app/components/cart/cart.component.ts
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { CartService } from '../../services/cart.service';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
@@ -11,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrl: './cart.component.css',
 })
 export class CartComponent {
+
+  @ViewChild('serviceCardContainer', { static: true }) serviceCardContainer!: ElementRef;
+
   constructor(public cartService: CartService) {}
+
+  scrollToService(serviceName: string): void {
+    const element = document.getElementById(serviceName);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
