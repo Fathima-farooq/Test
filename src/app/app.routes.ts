7f412008b193e2b5d32e7989103fb80c3356cfa8
Samreen Fathima Farooq <yoyosamreen@gmail.com>
2025-02-05 11:51:58 +0530

    presist cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ServiceCardComponent } from './components/service-card/service-card.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { StoreComponent } from './layout/store/store.component';
 
 export const routes: Routes = [
-    {path:"", component:AppComponent},
-    {path:"c", component:CartComponent},
-    {path:"sc", component:ServiceCardComponent},
+  { path: '', component: StoreComponent },
+  { path: 'checkout', loadComponent: () => import('./components/checkout/checkout.component').then(m => m.CheckoutComponent) }
 ];
