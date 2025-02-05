db6a5b667eafb445c89f47b9e69ec41920e039b1
Samreen Fathima Farooq <yoyosamreen@gmail.com>
2025-02-05 12:30:40 +0530

    the app which works actually

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { CarServiceService } from './services/car-service.service';
+import { ServiceModel } from './models/service.model';
+import { RouterOutlet } from '@angular/router';
+import { ServiceCardComponent } from './components/service-card/service-card.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CommonModule } from '@angular/common';
+import { ServiceCarouselComponent } from './components/service-carousel/service-carousel.component';
+
+interface GroupedServices {
+  [key: string]: ServiceModel[];
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <app-service-carousel
+      (categorySelect)="onCategorySelect($event)"
+    ></app-service-carousel>
+    <app-cart></app-cart>
+    <div class="container mt-1">
+      <div *ngFor="let group of Object.entries(groupedServices)">
+        <div class="section-title">{{ group[0] }}</div>
+        <ng-container *ngFor="let service of group[1]">
+          <app-service-card [service]="service"></app-service-card>
+        </ng-container>
+      </div>
+    </div>
+
+    <!-- Router outlet for pages like Checkout -->
+    <!-- <router-outlet></router-outlet> -->
+  `,
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterOutlet,
+    ServiceCardComponent,
+    CartComponent,
+    CommonModule,
+    ServiceCarouselComponent,
+  ],
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  services: ServiceModel[] = [];
+  groupedServices: GroupedServices = {};
+  protected readonly Object = Object;
+
+  constructor(private carService: CarServiceService) {}
+
+  ngOnInit() {
+    // Use the first category 'Periodic Services' by default
+    this.onCategorySelect('Periodic Services');
+  }
+
+  private groupServices() {
+    this.groupedServices = this.services.reduce((acc, service) => {
+      if (!acc[service.serviceHeading]) {
+        acc[service.serviceHeading] = [];
+      }
+      acc[service.serviceHeading].push(service);
+      return acc;
+    }, {} as GroupedServices);
+  }
+
+  onCategorySelect(category: string) {
+    this.carService.getServicesByCategory(category).subscribe({
+      next: (response: any) => {
+        this.services = response._embedded.productModels;
+        this.groupServices();
+      },
+      error: (error) => {
+        console.error('Error fetching category services:', error);
+      },
+    });
+  }
+}
